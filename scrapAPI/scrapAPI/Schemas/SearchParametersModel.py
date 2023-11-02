6421813418a1df95c0de3e0f0e971540fd16480a
Gonzalo Gonzalez <73320162+GonzaloGeh@users.noreply.github.com>
2023-11-02 14:34:24 -0300

    Delete scrapAPI/scrapAPI directory
    Re-push my own commitment.

--- scrapAPI/scrapAPI/Schemas/SearchParametersModel.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class SearchParametersModel(BaseModel):
-    baseUrl: str
-    firstName: str | None = None
-    lastName: str
-    county: str
-    yob: str
-    # ADD MORE PARAMETERS AS NEEDED
